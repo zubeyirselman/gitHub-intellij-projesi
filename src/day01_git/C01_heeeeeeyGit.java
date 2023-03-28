e3a6de4f5a52c45dec9d099273ed762e91968ddc
zsk <zsk199207@gmail.com>
2023-03-28 17:51:33 +0300

    2. gun sonu

--- src/day01_git/C01_heeeeeeyGit.java
+++ src/day01_git/C01_heeeeeeyGit.java
@@ -3,5 +3,7 @@ package day01_git;
 public class C01_heeeeeeyGit {
     public static void main(String[] args) {
         System.out.println("github sansini fazla zorlama");
+
+        System.out.println("var olan bu yeni satiri ve...");
     }
 }
---
+++ src/day01_git/C02_git.java
@@ -0,0 +1,7 @@
+package day01_git;
+
+public class C02_git {
+    public static void main(String[] args) {
+        System.out.println("bak haci bu da 2. class");
+    }
+}
